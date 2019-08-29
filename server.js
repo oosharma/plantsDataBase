c4d03e112e4d07266932b99c1749a8f634f62b1b
Abhishek Sharma <abhisheksharma@Abhisheks-MBP.local>
2019-08-29 01:25:40 -0700

    updated scripts

--- server.js
+++ server.js
@@ -89,6 +89,12 @@ router.post("/putData", (req, res) => {
   });
 });
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
 // append /api for our http requests
 app.use("/api", router);
 
